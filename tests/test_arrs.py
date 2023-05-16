8df216d82665b9ec2207422beb0bcae6b959a271
irinasiur <drozdovairina10@gmail.com>
2023-05-16 18:51:52 +0300

    Add tests to test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,12 +2,19 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
-    assert arrs.get([], 2, "test") == "test"
-    assert arrs.get([1, 2, 3], None, "test") == "test"
+    self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+    self.assertEqual(arrs.get([], 0, "test"), "test")
+    self.assertEqual(arrs.get([], 2, "test"), "test")
+    self.assertEqual(arrs.get([1, 2, 3], None, "test"), "test")
+    self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
+    self.assertEqual(arrs.get([1, 2], 2, "test"), "test")
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(self):
+    self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+    self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+    self.assertEqual(arrs.my_slice([], 0), [])
+    self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, None), [2, 3, 4])
+    self.assertEqual(arrs.my_slice([1, 2, 3, 4], -1, 3), [])
+    self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, -3), [])
+    self.assertEqual(arrs.my_slice([1, 2], -3), [1, 2])
